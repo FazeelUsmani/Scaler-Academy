c18a1a162cf4303f54c61f3387908544df6d1b3d
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-03-07 12:46:45 +0530

    Update Notes.cpp

--- 014 Two Pointers/Notes.cpp
+++ 014 Two Pointers/Notes.cpp
@@ -66,3 +66,39 @@ int main(){
 
 
 
+
+3) Given a sorted array, return i,j,k for which A[i+A[j]+A[k] = x    
+    
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    vector<int> A{1,3,5,10,20,23,30};
+    int n = A.size();
+    int x = 38;
+
+    for (int i = 0; i < n; i++){
+
+        int ans =  x - A[i];
+        int j = 0, k = n-1;
+
+        while (j<n && k>=0){
+            int sum = A[j]+A[k];
+
+            if (sum == ans){
+                cout<<" Indices are "<<i<<" "<<j<<" "<<k<<" having values "<<A[i]<<" + "<<A[j]<<" + "<<A[k]<<" = "<<x<<endl;
+                break;
+            }
+
+            else if (sum > ans)
+                k--;
+
+            else
+                j++;
+        }       
+    }
+    
+    return 0;
+}
+
