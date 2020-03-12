a46b2d75fca76f9552875908ce7d33d3288a8bfd
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-03-12 12:29:32 +0530

    Update Notes.py

--- 015 Hashing 1/Notes.py
+++ 015 Hashing 1/Notes.py
@@ -1,4 +1,4 @@
-1) Given an array, Return True if there exists a sub array such that sum of elements of subarray is zero else False
+# 1) Given an array, Return True if there exists a sub array such that sum of elements of subarray is zero else False
 
 arr = [2,4,-3,-1,5,-1]
 
@@ -10,7 +10,8 @@ def subarray(arr):
   x.add(pre)
   for i in range(1,n):    
     currSum = pre + arr[i]
-    
+    if currSum == 0:        # Because the prefix Sum is 0 iff the sum of elements from 0 to that element is 0
+      return True
     if currSum in x:
       return True
     else:
@@ -22,3 +23,74 @@ def subarray(arr):
 
 
 print(subarray(arr))
+
+
+_______________________________________________________________________________________________________
+
+# 2) Given an array, return the length of longest subarray of sum zero
+
+# Create a dictionary of prefixSum and the first occurance index of an element
+
+arr = [2,4,-3,-1,5,-1]
+
+def subarray1(arr):
+  n = len(arr)
+  dict = {}
+  pre = arr[0]
+  currSum = 0
+  maxLength = -1
+  dict[pre] = 0
+
+  for i in range(1, n):
+    
+    currSum = pre + arr[i]
+    if currSum in dict:
+      maxLength = max(maxLength, i - dict[currSum] )
+    else:
+      dict[currSum] = i
+      
+    pre = currSum
+
+  return maxLength
+
+print(subarray1(arr))
+
+
+
+# What if the input array is [5,-4,-1]? Does the above solution work?
+# a) No, isn't it? This is the corner case you need to handle it
+
+arr = [5,-4,-1,7]
+
+def subarray1(arr):
+  n = len(arr)
+  dict = {}
+  pre = arr[0]
+  currSum = 0
+  maxLength = -1
+  dict[pre] = 0
+
+  for i in range(1, n):
+         
+    currSum = pre + arr[i]
+
+    if currSum == 0:                              # I have added these two lines of code to handle cornercase
+      maxLength = max(maxLength, i+1)
+    
+    if currSum in dict:
+      maxLength = max(maxLength, i - dict[currSum] )
+    else:
+      dict[currSum] = i
+    pre = currSum
+
+  return maxLength
+
+print(subarray1(arr))
+
+_______________________________________________________________________________________________________
+
+
+
+
+
+
