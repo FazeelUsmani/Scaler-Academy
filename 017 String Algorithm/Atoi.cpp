db7753a8117341918927d58fef3e74db08d9c4d6
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-03-20 16:36:08 +0530

    Update Atoi.cpp

--- 017 String Algorithm/Atoi.cpp
+++ 017 String Algorithm/Atoi.cpp
@@ -3,30 +3,27 @@ using namespace std;
 
 int atoi(string str){
 
-   int sign = 1, base = 0, i = 0;
-    while (str[i] == ' ')
-        i++; 
-
-    if (str[i] == '-' || str[i] == '+') 
+    int sign = 1, base = 0, i = 0;
+    while (str[i] == ' ') { i++; }
+    
+    if (str[i] == '-' || str[i] == '+') {
         sign = (str[i++] == '-') ? -1 : 1; 
-        
-
+    }
+    
     while (str[i] >= '0' && str[i] <= '9') {
-
-        if (base >  (INT_MAX-str[i]) / 10) 
+        cout<<base<<endl;
+        if (base >  INT_MAX / 10 || (base == INT_MAX / 10 && str[i] - '0' > 7)) {
             if (sign == 1) return INT_MAX;
             else return INT_MIN;
-        
+        }
         base  = 10 * base + (str[i++] - '0');
     }
-
     return base * sign;
-
     }
 
 int main(){ 
 
-    string s = "7 U 0 T7165 0128862 089 39 5";
+    string s = "2147483646";
     cout<<atoi(s);
 
     return 0;
