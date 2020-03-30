ecf92a07faa38be57026e0572eb7d3315aa9059f
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-03-30 23:15:47 +0530

    Create circularLL.cpp

---
+++ 022 Linked List 1/circularLL.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// We can traverse the whole list from any node
+// Insert at begin or end in O(1) by just maintaining one pointer
+// Can implement a queue
+// Implementing Scheduling algorithms like Round Robin
+
+
+struct Node{
+    int data;
+    Node *next;
+    Node (int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// While inserting make sure that the last element points to the head
+
+int main(){
+
+
+
+    return 0;
+}
