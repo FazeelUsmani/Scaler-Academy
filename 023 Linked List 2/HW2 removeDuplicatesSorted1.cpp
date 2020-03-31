155aa6697bcb57ac46a02efc10411769788de7f8
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-03-31 23:10:01 +0530

    Homework

---
+++ 023 Linked List 2/HW2 removeDuplicatesSorted1.cpp
@@ -0,0 +1,67 @@
+/*
+Remove Duplicates from Sorted List
+
+Problem Description
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+
+Problem Constraints
+0 <= length of linked list <= 1000000
+
+
+Input Format
+First argument is the head pointer of the linked list.
+
+
+Output Format
+Return the head pointer of the linked list after removing all duplicates.
+
+
+Example Input
+Input 1:
+1->1->2
+Input 2:
+1->1->2->3->3
+ 
+
+
+Example Output
+Output 1:
+1->2
+Output 2:
+1->2->3
+ 
+
+
+Example Explanation
+Explanation 1:
+each element appear only once in 1->2.
+*/
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* Solution::deleteDuplicates(ListNode* A) {
+    
+    ListNode *temp = A;
+    while (temp != NULL && temp->next != NULL){
+        
+        if (temp->val == temp->next->val){
+            ListNode *x = temp->next;
+            temp->next = x->next;
+            free(x);
+        }
+        else
+            temp = temp->next;
+    
+    }
+    
+    return A;
+}
