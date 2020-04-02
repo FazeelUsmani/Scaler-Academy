520971a7db03202656412daadb65a9366859385c
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-04-02 23:18:38 +0530

    Assignment

---
+++ 017 String Algorithm/A3 reverseString.py
@@ -0,0 +1,53 @@
+/*
+Reverse the String
+Problem Description
+Given a string A of size N. Return the string A after reversing the string word by word. NOTE:
+A sequence of non-space characters constitutes a word.
+Your reversed string should not contain leading or trailing spaces, even if it is present in the input string.
+If there are multiple spaces between words, reduce them to a single space in the reversed string.
+ 
+
+
+Problem Constraints
+1 <= N <= 105
+
+
+Input Format
+The only argument given is string A.
+
+
+Output Format
+Return the string A after reversing the string word by word.
+
+
+Example Input
+Input 1:
+    A = "the sky is blue"
+Input 2:
+    A = "this is ib"  
+
+
+Example Output
+Output 1:
+    "blue is sky the"
+Output 2:
+    "ib is this"    
+
+
+Example Explanation
+Explanation 1:
+    We reverse the string word by word so the string becomes "the sky is blue".
+
+
+*/
+
+class Solution:
+    # @param A : string
+    # @return a strings
+    def solve(self, A):
+        
+        
+        A = A.split()
+        A = A[::-1]
+        return ' '.join(A)
+        
