89d3438ec124b375f3221adf6bbcf15b18901fb4
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-04-04 00:02:58 +0530

    Update tree.cpp

--- basics/tree.cpp
+++ basics/tree.cpp
@@ -1,6 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct Node{
+    int data;
+    Node *left;
+    Node *right;
+    Node (int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+
 int main(){
 
     Node *a = new Node(10);
