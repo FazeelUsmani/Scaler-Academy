cdc2d3ec7665b866ede9760a1279b50a62235a6f
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-04-03 23:58:30 +0530

    without using recursion

---
+++ 024 Trees 1/A2 InorderTraversal.cpp
@@ -0,0 +1,50 @@
+/*
+Inorder Traversal:
+
+Given a binary tree, return the inorder traversal of its nodes' values. 
+
+Example : Given binary tree
+
+   1
+    \
+     2
+    /
+   3
+   
+return [1,3,2]. 
+Using recursion is not allowed.
+*/
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+vector<int> Solution::inorderTraversal(TreeNode* A) {
+    
+    stack <TreeNode *> s;
+    TreeNode *x = A;
+    vector <int> ans;
+    
+    
+    while (!s.empty() || x != NULL){
+        
+        while (x != NULL){
+            s.push(x);
+            x = x->left;
+        }
+        
+        x = s.top();
+        s.pop();
+            
+        ans.push_back( x -> val );
+        
+        x = x->right;
+    }
+    
+    return ans;
+}
