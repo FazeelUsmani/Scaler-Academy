495a1b39e9fe7ace02cbc72bcf0442db165aef76
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-04-03 23:59:18 +0530

    printing the max element

---
+++ 024 Trees 1/getMax.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int getMax(Node *root){
+    if (root == NULL)
+        return INT_MIN;
+    else
+        return max(root->data, max(getMax(root->left), getMax(root->right)));
+    
+}
+
+int main(){
+
+    Node *a = new Node(10);
+
+    a->left = new Node(20);
+    a->right = new Node(30);
+
+    a->left->left = new Node(40);
+    a->left->right = new Node(50);
+
+    a->right->left = new Node(60);
+    a->right->right = new Node(70);
+    
+    cout<<" \nMax is "<<getMax(a);
+
+    return 0;
+}
