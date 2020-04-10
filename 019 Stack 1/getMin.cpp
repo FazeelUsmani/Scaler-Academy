b7aee99cc34238ee4b42d38a876725ba2d53d2cf
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-04-10 16:39:34 +0530

    implementing using auxillary stack     SC : O(2*N)

--- 019 Stack 1/getMin.cpp
+++ 019 Stack 1/getMin.cpp
@@ -1,110 +1,47 @@
-
-#include <cstdlib>
-#include <iostream>
-using namespace std;
-
-#define SIZE 10
-
-class stack{
-    int *arr;
-    int top; 
-    int capacity;
-    int minEle;
-
-    public:
-        stack(int size = SIZE);
-        ~stack();
-
-        void push(int);
-        int pop();
-
-        int peek();
-        int size();
-
-        bool isEmpty();
-        bool isFull();
-        int getMin();
-
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    vector<int> st;
+    vector<int> minStack;
+    
+    MinStack() {
+        st.clear();
+        minStack.clear();
+    }
+    
+    void push(int x) {
+        st.push_back(x);
+        if ( (minStack.size() == 0) || (minStack.back() >= x) )
+            minStack.push_back(x);        
+    }
+    
+    void pop() {
+        if (st.size() == 0)
+            return;
+        int x = st.back();
+        st.pop_back();
+        if (minStack.back() == x)
+            minStack.pop_back();        
+    }
+    
+    int top() {
+        if (st.size() == 0)
+            return -1;
+        return st.back();
+    }
+    
+    int getMin() {
+        if (minStack.size() > 0)
+            return minStack.back();
+        return -1;        
+    }
 };
 
-stack::stack(int size){
-    arr = new int[size];
-    capacity = size;
-    top = -1;
-    minEle = 10000;
-}
-
-stack::~stack(){
-    delete arr;
-}
-
-
-void stack::push(int x){
-    if (isFull()){
-        cout<<"Overflow";
-        //exit(EXIT_FAILURE);
-    }
-    cout<<" Inserting "<<x<<endl;
-    if (x <= minEle){
-        arr[++top] = x-minEle;
-        minEle = x;
-    }
-    else{
-        arr[++top] = x;
-    }
-}
-
-int stack::pop(){
-    if (isEmpty()){
-        cout<<"Underflow ";
-        //exit(EXIT_FAILURE);
-    }
-    cout<<"Removing "<<peek()<<endl;
-    if (arr[top] < 0){
-        minEle = minEle - arr[top];
-    }
-        
-    return arr[top--];
-}
-
-int stack::peek(){
-    if (isEmpty()){
-        cout<<"No element in stack\n";
-        //exit(EXIT_FAILURE);
-    }
-    return arr[top];
-}
-
-int stack::size(){
-    return top+1;
-}
-    
-
-bool stack::isEmpty(){
-    return top == -1;
-}
-
-bool stack::isFull(){
-    return top == capacity-1;
-}
-
-int stack::getMin(){
-    if (top == -1)
-        return -1;
-    cout<<" Top Index "<<top<<" \n Min Element is ";
-    return minEle;
-}
-
-
-int main(){
-
-    stack st(10);    
-
-    st.push(2);
-    st.push(1);
-    cout<<st.peek();
-    
-    return 0;
-}
-
-
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
