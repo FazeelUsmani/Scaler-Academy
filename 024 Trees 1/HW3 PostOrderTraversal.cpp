40b98d7fd8e71acb4da088a7b884e24223a74034
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-05-01 01:19:59 +0530

    includes Aliter

--- 024 Trees 1/HW3 PostOrderTraversal.cpp
+++ 024 Trees 1/HW3 PostOrderTraversal.cpp
@@ -42,5 +42,31 @@ vector<int> Solution::postorderTraversal(TreeNode* A) {
         
     }
     reverse (arr.begin(), arr.end());
+   
+    return arr;
+}
+
+// Aliter:
+vector<int> Solution::postorderTraversal(TreeNode* A) {
+    
+    stack <TreeNode *> s;
+    TreeNode *curr = A;
+    vector<int> arr;
+    
+    while (!s.empty() || curr != NULL){
+        
+        if (curr != NULL){
+            arr.push_back(curr->val);
+            s.push(curr);
+            curr = curr->right;
+        }
+        else{
+            TreeNode *temp = s.top();
+            s.pop();
+            curr = temp->left;
+        }
+    }
+    reverse (arr.begin(), arr.end());
+    
     return arr;
 }
