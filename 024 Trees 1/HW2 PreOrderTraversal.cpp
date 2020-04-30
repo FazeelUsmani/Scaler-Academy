49a46a0b0c78547f51d5012dae4f37851aee647d
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-05-01 00:54:48 +0530

    includes aliter

--- 024 Trees 1/HW2 PreOrderTraversal.cpp
+++ 024 Trees 1/HW2 PreOrderTraversal.cpp
@@ -38,3 +38,28 @@ vector<int> Solution::preorderTraversal(TreeNode* A) {
     
     return arr;
 }
+
+
+// Aliter: 
+
+vector<int> Solution::preorderTraversal(TreeNode* A) {
+
+    stack <TreeNode *> s;
+    TreeNode *curr = A;
+    vector<int> arr;
+
+    while (!s.empty() || curr != NULL){
+        if (curr != NULL){
+            arr.push_back(curr->val);
+            s.push(curr);
+            curr = curr->left;
+        }
+        else{
+            TreeNode *temp = s.top();
+            s.pop();
+            curr = temp->right;
+        }
+    }
+    
+    return arr;
+}
