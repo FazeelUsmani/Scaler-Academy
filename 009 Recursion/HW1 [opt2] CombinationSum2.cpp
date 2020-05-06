2de4801919304089c0111c50ce1b77ea3e1293a9
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-05-06 16:52:26 +0530

    pruning the valiues which give duplicate answers

---
+++ 009 Recursion/HW1 [opt2] CombinationSum2.cpp
@@ -0,0 +1,33 @@
+vector<vector<int> > ans;
+
+void recurse(vector<int> &A, int B, vector<int> &curr, int sum, int index){
+    
+    if (sum == B){
+        ans.push_back(curr);
+        return;
+    }
+    if (sum > B)
+        return;
+        
+    int freq[10000] {0};
+    for (int i = index; i < A.size(); i++){
+        if (freq[A[i]] == 0){
+            freq[A[i]] = 1;
+            curr.push_back(A[i]);
+            recurse(A,B,curr,sum+A[i],i+1);
+            curr.pop_back();
+        }
+    }
+    
+    return;
+}
+
+vector<vector<int> > Solution::combinationSum(vector<int> &A, int B) {
+    
+    ans.clear();
+    sort(A.begin(), A.end());
+    vector<int> curr;
+    recurse(A, B, curr, 0, 0);
+    
+    return ans;
+}
