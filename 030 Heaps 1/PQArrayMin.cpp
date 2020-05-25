3f5a56f32bfdbf00af4e201c11dcb14f7a0b4c20
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-05-26 00:14:51 +0530

    Build Min Heap TC: O(N) better than pushing elements one by one

---
+++ 030 Heaps 1/PQArrayMin.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int arr[] = {10, 15, 4};
+
+    for (int &x : arr)
+        x = -x;
+
+    priority_queue<int> pq(arr, arr+3);    // For vector pass v.begin(), v.end() 
+    // This way is better instead of pushing elements one by one i.e. Build Heap
+    // push and pop O(log N)
+    // empty, size and top is O(1)
+
+    while (!pq.empty()){
+        cout<<-pq.top()<<" ";
+        pq.pop();
+    }
+
+
+    return 0;
+}
