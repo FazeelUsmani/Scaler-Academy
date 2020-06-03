273fc96cf5029c009dca1b06866a23e57ce9254d
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-06-03 21:07:26 +0530

    memoized solution

---
+++ 034 Dynamic Programming (1D DP)/A5 FibonacciNumber.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+int *dp;
+
+int fib(int n){
+    if (dp[n] == -1){
+        int res;
+        if (n <= 0)
+            res = 0;
+        else if (n == 1)
+            res = 1;
+        else
+            res = fib(n-1)+fib(n-2);
+        dp[n] = res;
+    }
+    
+    return dp[n];
+}
+
+
+int main()  {
+    
+    int n;
+    cin>>n;
+    dp = new int[n+1];
+    for (int i = 0; i <= n; i++)
+        dp[i] = -1;
+        
+    cout<<fib(n)<<endl;
+    
+    return 0;
+}
