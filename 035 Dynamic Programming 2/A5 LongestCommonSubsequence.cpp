31df404204d259428f2c223e0fad75f2752ba07d
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-06-07 23:27:05 +0530

    Create A5 LongestCommonSubsequence.cpp

---
+++ 035 Dynamic Programming 2/A5 LongestCommonSubsequence.cpp
@@ -0,0 +1,59 @@
+/*
+
+Longest Common Subsequence
+
+Problem Description
+
+Given two strings A and B. Find the longest common subsequence ( A sequence which does not need to be contiguous), 
+which is common in both the strings.
+
+You need to return the length of such longest common subsequence.
+
+
+
+Problem Constraints
+1 <= Length of A, B <= 1005
+
+
+
+Input Format
+First argument is a string A.
+Second argument is a string B.
+
+
+
+Output Format
+Return an integer denoting the length of the longest common subsequence.
+
+
+
+Example Input
+Input 1:
+
+ A = "abbcdgf"
+ B = "bbadcgf"
+Input 2:
+
+ A = "aaaaaa"
+ B = "ababab"
+
+
+Example Output
+Output 1:
+
+ 5
+Output 2:
+
+ 3
+
+
+Example Explanation
+Explanation 1:
+
+ The longest common subsequence is "bbcgf", which has a length of 5.
+Explanation 2:
+
+ The longest common subsequence is "aaa", which has a length of 3.
+
+
+*/
