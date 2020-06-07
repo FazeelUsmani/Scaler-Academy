2ddda4e730caaec456b54dca2c3575fbdbf99d7a
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-06-07 23:27:26 +0530

    Assignment

--- 035 Dynamic Programming 2/A5 LongestCommonSubsequence.cpp
+++ 035 Dynamic Programming 2/A5 LongestCommonSubsequence.cpp
@@ -57,3 +57,36 @@ Explanation 2:
 
 
 */
+
+
+int Solution::solve(string s1, string s2) {
+    
+    int m = s1.length(), n = s2.length();
+    int dp[m+1][n+1];
+
+    // first row as 0, because any char with " " matches 0 length
+    for (int i = 0; i <= m; i++)
+        dp[i][0] = 0;
+
+    // first col as 0, because any char with " " matches 0 length
+    for (int j = 0; j <= n; j++)
+        dp[0][j] = 0;
+
+    for (int i = 1; i <= m; i++){
+        for (int j = 1; j <= n; j++){
+            if (s1[i-1] == s2[j-1]){
+                dp[i][j] = 1 + dp[i-1][j-1];  // taking diagonal value
+            }
+            else
+            {
+                dp[i][j] = max(dp[i][j-1], dp[i-1][j]);  // taking max of left and top cell
+            }
+            
+        }
+    }
+
+    
+ 
+    return dp[m][n];
+}
+
