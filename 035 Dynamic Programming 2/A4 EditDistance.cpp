22fd250566935d2cd909db9bcc84877b3f2ad02f
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-06-12 00:54:33 +0530

    Create A4 EditDistance.cpp

---
+++ 035 Dynamic Programming 2/A4 EditDistance.cpp
@@ -0,0 +1,25 @@
+int Solution::minDistance(string word1, string word2) {
+    
+    int m = word1.size(), n = word2.size();
+    int dp [m+1][n+1];
+    
+    for (int i = 0; i <= m; i++)
+        dp[i][0] = i;
+    
+    for (int j = 0; j <= n; j++)
+        dp[0][j] = j;
+    
+    for (int i = 1; i <= m; i++){
+        for (int j = 1; j <= n; j++){
+            if (word1[i-1] == word2[j-1])
+                dp[i][j] = dp[i-1][j-1];
+            
+            else
+                dp[i][j] = 1 + min(dp[i][j-1], min(dp[i-1][j], dp[i-1][j-1]));
+            
+        }
+    }
+    
+    return dp[m][n];
+    
+}
