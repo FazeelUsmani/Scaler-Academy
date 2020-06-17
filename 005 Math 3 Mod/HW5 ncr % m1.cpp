d67e037792e82dc7db23090e2395ecf77bb356b3
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-06-18 00:54:24 +0530

    [OPTIMZED] SC: O(r)

---
+++ 005 Math 3 Mod/HW5 ncr % m1.cpp
@@ -0,0 +1,12 @@
+long long int combination(int n,int r,int mod){
+    int C[r+1];
+    memset(C, 0, sizeof(C));
+ 
+    C[0] = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = min(i, r); j > 0; j--)
+            C[j] = (C[j] + C[j-1])%mod;
+    }
+    return C[r];
+}
