091319a8aece1effdb10ee508312bd8895fc4497
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-08-22 17:47:43 +0530

    rod cutting

--- 050 Practice/04 DP Remedial/3_cutRod.cpp
+++ 050 Practice/04 DP Remedial/3_cutRod.cpp
@@ -1,23 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int maxCuts(int A, vector<int> &B, int N, int cost){
-
-    if (N == 1)
-        return B[1];
-    // cout<<N<<" "<<cost<<endl;
-    for (int i = 0; i < N; ++i){
-        cost = B[i] + maxCuts(A, )
-    }
+int maxCuts(int A, vector<int> &B){
+   
     
-    return cost;
+    int maxCut[A+1];
+    maxCut[0] = 0;
+    int i, j;
+    
+    for (i = 1; i <= A; ++i){
+        int maxVal = INT_MIN;
+        for (j = 0; j < i; ++j)
+            maxVal = max(maxVal, B[j] + maxCut[i-j-1]);
+        maxCut[i] = maxVal;
+    }
+
+    return maxCut[A];
 }
 
 int main(){   
 
     int A = 5;
     vector<int> B{3,7,7,10,12};
-    cout<<maxCuts(A, B, A, 0);
+    cout<<maxCuts(A, B);
 
     return 0;
 }
