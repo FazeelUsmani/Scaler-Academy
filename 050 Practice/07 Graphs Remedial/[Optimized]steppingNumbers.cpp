721eff820058a1af212ff2689be747de9da7c9d0
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-08-25 23:34:20 +0530

    TC: O(NlogN)

---
+++ 050 Practice/07 Graphs Remedial/[Optimized]steppingNumbers.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> ans;
+void bfs(int n, int m, int num){
+    queue<int> q;
+    q.push(num);
+
+    while (!q.empty()){
+        int stepNum = q.front();
+        q.pop();
+
+        if (stepNum >= n && stepNum <= m)
+            ans.push_back(stepNum);
+
+        if (num == 0 || stepNum > m)
+            continue;
+        
+        int lastDigit = stepNum%10;
+        int stepNumA = stepNum*10 + (lastDigit-1);
+        int stepNumB = stepNum*10 + (lastDigit+1);
+
+        if (lastDigit==0)
+            q.push(stepNumA);
+        else if (lastDigit==9)
+            q.push(stepNumB);
+        else{
+            q.push(stepNumA);
+            q.push(stepNumB);
+        }
+    }
+    //cout<<"complete\n";
+}
+
+
+int main(){   
+
+    ans.clear();
+    int n = 0, m = 30;
+    for (int i = 0; i < 10; ++i)
+        bfs(n, m, i);
+
+    //sort(ans.begin(), ans.end());
+
+    for (int &x: ans)
+        cout<<x<<" ";
+
+    return 0;
+}
