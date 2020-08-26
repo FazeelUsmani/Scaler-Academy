eee3ad9ebaf467960dee397d315668f1af4c2704
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-08-27 00:05:17 +0530

    adding two LL

--- 022 Linked List 1/addLL.cpp
+++ 022 Linked List 1/addLL.cpp
@@ -1,39 +1,133 @@
-#include <bits/stdc++.h>
+// A recursive program to add two linked lists 
+#include <iostream> 
 using namespace std;
 
-struct Node{
-    int data;
-    Node *next;
-    Node (int x){
-        data = x;
-        next = NULL;
-    }
-};
+struct Node 
+{ 
+	int data; 
+	struct Node* next; 
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+}; 
 
-Node *insertEnd(Node *head, int data){
-    if (head == NULL)
-        return new Node(data);
-
-    Node *temp = head;
-    while (temp -> next != NULL){
-        temp = temp -> next;
-    }
-
-    temp->next = new Node(data);
-    return head;
+void append(Node** head_ref, Node **tail_ref, int new_data)
+{
+    struct Node* new_node = new Node(new_data);
+    
+    if (*head_ref == NULL)
+        *head_ref = new_node;
+    else
+        (*tail_ref)->next = new_node;
+    *tail_ref = new_node;
 }
 
-void printLL(Node *head){
-    cout<<"printLL() ";
-    while (head != NULL){
-        cout<< head->data <<" ";
-        head = head -> next;
-    }
-    cout<<endl;
-}
+void push(Node** head_ref, int new_data) 
+{ 
+    /* allocate node */
+    Node* new_node = new Node(new_data); 
+  
+    /* link the old list off the new node */
+    new_node->next = (*head_ref); 
+  
+    /* move the head to point to the new node */
+    (*head_ref)    = new_node; 
+} 
 
-Node* addSameSize(Node* head1, Node* head2, int* carry){
+/* A utility function to print linked list */
+void printList(Node *node) 
+{ 
+	while (node != NULL) 
+	{ 
+		cout<<node->data<<" ";
+		node = node->next; 
+	} 
+	cout<<endl;
+} 
 
+// A utility function to swap two pointers 
+void swapPointer( Node** a, Node** b ) 
+{ 
+	Node* t = *a; 
+	*a = *b; 
+	*b = t; 
+} 
+
+Node* addSameSize(Node* head1, Node* head2, int* carry) ;
+
+void addCarryToRemaining(Node* head1, Node* curr, int* carry, Node** result) ;
+
+
+// The main function that adds two linked lists represented by head1 and head2. 
+// The sum of two lists is stored in a list referred by result 
+void addList(Node* head1, Node* head2, int size1, int size2, Node** result) 
+{ 
+	Node *cur; 
+    if (head1 == NULL) { 
+		*result = head2; 
+		return; 
+	}
+
+	if (head2 == NULL) 
+	{ 
+		*result = head1; 
+		return; 
+	}
+	
+	int carry = 0; 
+
+	if (size1 == size2) 
+		*result = addSameSize(head1, head2, &carry); 
+    else
+	{ 
+		int diff = abs(size1 - size2); 
+
+		if (size1 < size2) 
+			swapPointer(&head1, &head2); 
+
+		for (cur = head1; diff--; cur = cur->next);
+		
+        *result = addSameSize(cur, head2, &carry); 
+        
+		addCarryToRemaining(head1, cur, &carry, result); 
+	} 
+
+	if (carry) 
+		push(result, carry); 
+} 
+
+// Driver program to test above functions 
+int main() 
+{ 
+	int T;
+	cin>>T;
+	while(T--){
+	    Node *head1 = NULL, *tail1 = NULL;
+	    Node *head2 = NULL, *tail2 = NULL;
+	    Node *result = NULL; 
+        int size1, size2;
+        cin>>size1>>size2;
+        
+        int temp;
+        for(int i = 1; i<=size1; i++){
+            cin>>temp;
+            append(&head1, &tail1, temp);
+        }
+        for(int i = 1; i<=size2; i++){
+            cin>>temp;
+            append(&head2, &tail2, temp);
+        }
+        
+        addList(head1, head2, size1, size2, &result); 
+        printList(result); 
+	}
+    return 0; 
+} 
+
+Node* addSameSize(Node* head1, Node* head2, int* carry) 
+{ 
     Node* temp;
     int car = 0;
     
@@ -46,34 +140,16 @@ Node* addSameSize(Node* head1, Node* head2, int* carry){
     (*carry) = car/10;
     
     return head2;
-}
+} 
 
 void addCarryToRemaining(Node* head1, Node* curr, int* carry, Node** result) 
 { 
-    
-	
-}
-
-int main(){
-
-    Node *head1 = NULL, *head2 = NULL;
-
-    head1 = insertEnd(head1, 9);
-    head1 = insertEnd(head1, 8);
-    head1 = insertEnd(head1, 9);
-    printLL(head1);
-
-    head2 = insertEnd(head2, 4);
-    head2 = insertEnd(head2, 5);
-    head2 = insertEnd(head2, 6);
-    printLL(head2);
-    
-    int carry = 0;
-    Node *res = addSameSize(head1, head2, &carry);
-    
-    printLL(res);
-        
-
-
-    return 0;
+	if (head1 == curr)
+	    return;
+	Node *temp = new Node(0);
+	addCarryToRemaining(head1->next, curr, carry, result);
+	temp->next = *result;
+	temp->data = (head1->data + *carry)%10;
+	*carry = (head1->data + *carry)/10;
+	*result = temp;
 }
