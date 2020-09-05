a94f467b1c287e459fd6f5052eb7f58ec355016a
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-09-05 20:01:09 +0530

    Catalan Number using recursion

---
+++ 050 Practice/05 DP 2 (Remedial)/1 uniqueBST.cpp
@@ -0,0 +1,11 @@
+int Solution::numTrees(int A) {
+    
+    if (A <= 1)
+        return 1;
+    
+    unsigned long int ans = 0;
+    for (int i = 0; i < A; ++i)
+        ans += numTrees(i)*numTrees(A-i-1);
+    
+    return ans;
+}
