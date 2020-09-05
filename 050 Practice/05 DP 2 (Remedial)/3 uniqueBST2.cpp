d2ee75d9e15bcc2f63021241a5d322e63a075be4
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-09-05 20:50:21 +0530

    TC:O(N) using binomial coefficient

---
+++ 050 Practice/05 DP 2 (Remedial)/3 uniqueBST2.cpp
@@ -0,0 +1,28 @@
+typedef long long ll;
+
+// Binomial Coeefficient C(n, k)
+ll binomialCoeff(int n, int k){
+    
+    ll res = 1;
+    // Since C(n, k) = C(n, n-k)
+    if (k > n-k)
+        k = n-k;
+    
+    for (int i = 0; i < k; ++i){
+        res *= (n-i);
+        res /= (i+1);
+    }
+    
+    return res;
+}
+
+int Solution::numTrees(int A) {
+    
+    // Use below formula to calculate catalan number in O(N) time
+    // C_n=\frac{1}{n+1}\binom{2n}{n}
+    // ans = (1/(2nCn))
+    
+    ll c = binomialCoeff(2*A, A);
+    
+    return c/(A+1);
+}
