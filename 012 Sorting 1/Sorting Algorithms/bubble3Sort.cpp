0d9a9c6522df8aec760cc057a3aede13a476c75d
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-09-09 16:41:12 +0530

    odd & even indexed elements sorted seperately

---
+++ 012 Sorting 1/Sorting Algorithms/bubble3Sort.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Sorting elements alternatively
+// Odd indexed and even indexed elements are sorted seperately
+
+void bubbleSort (vector<int> A) {
+    
+    int n = A.size();
+    for (int i = 0; i < n-1; ++i){
+        for (int j = 0; j < n-i-2; ++j){
+            if (A[j] > A[j+2])
+                swap(A[j], A[j+2]);
+        }       
+    }
+
+    for (int x : A)
+        cout<<x<<" ";
+}
+
+int main(){   
+    vector<int> A {4,3,5,6,7,2};
+    bubbleSort(A);
+
+    return 0;
+}
