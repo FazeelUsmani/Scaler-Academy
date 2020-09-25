b9e3f59b9abfa684bf716448cf958ff60a3dbf84
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-09-25 17:57:04 +0530

    Create HW5 minSwaps2.cpp

---
+++ 013 Sorting 2/HW5 minSwaps2.cpp
@@ -0,0 +1,70 @@
+/*
+
+Minimum Swaps 2
+Problem Description
+
+Given an array of integers A of size N that is a permutation of [0, 1, 2, ..., (N-1)]. It is allowed to swap any two elements (not necessarily consecutive).
+
+Find the minimum number of swaps required to sort the array in ascending order.
+
+
+
+Problem Constraints
+1 <= N <= 100000
+0 <= A[i] < N
+
+
+
+Input Format
+The only argument given is the integer array A.
+
+
+
+Output Format
+Return the minimum number of swaps.
+
+
+
+Example Input
+Input 1:
+
+A = [1, 2, 3, 4, 0]
+Input 2:
+
+A = [2, 0, 1, 3]
+
+
+Example Output
+Output 1:
+
+4
+Output 2:
+
+2
+
+
+Example Explanation
+Explanation 1:
+
+ If you swap (1, 2) -> (2, 3) -> (4, 0) -> (3, 0). You will get a sorted array.
+ You cannot sort it with lesser swaps.
+Explanation 2:
+
+ You cannot sort it with lesser than 2 swaps.
+ */
+
+int Solution::solve(vector<int> &A) {
+    
+    int cnt=0, temp, temp2;
+    for (int i = 0; i < A.size(); ++i) {
+        while (A[i] != i) {
+            temp = A[i];
+            temp2 = A[A[i]];
+            A[i] = temp2;
+            A[temp] = temp;
+            cnt++;
+        }
+    }
+    
+    return cnt;
+}
