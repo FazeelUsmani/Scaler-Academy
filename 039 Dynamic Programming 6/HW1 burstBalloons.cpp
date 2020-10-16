8d815b4911eb879d88cc4b390c45d23ae7685712
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-10-16 23:55:10 +0530

    added solution

--- 039 Dynamic Programming 6/HW1 burstBalloons.cpp
+++ 039 Dynamic Programming 6/HW1 burstBalloons.cpp
@@ -62,3 +62,20 @@ Explanation 2:
  Total coins collected = 6 + 6 + 3 = 15
 
 */
+
+int go( int l, int r, vector<int> &A, vector<vector<int> > &dp ){
+    if( l > r )return 0;
+    int &ans = dp[l][r];
+    if( ans != -1 )return ans;
+    for( int i = l; i <= r; i++ ){
+        ans = max( ans, go(l, i-1, A, dp)+go(i+1, r, A, dp)+(l==0?1:A[l-1])*A[i]*(r==A.size()-1?1:A[r+1]) );
+    }
+    return ans;
+}
+
+
+int Solution::solve(vector<int> &A) {
+    vector<vector<int> > dp(A.size(), vector<int>(A.size(), -1));
+    
+    return go(0, A.size()-1, A, dp);
+}
