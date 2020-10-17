e2eea1b7c742f72f3577027149ddfef669333f30
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-10-17 23:53:28 +0530

    added solution

--- 038 Dynamic Programming 5/A2 0-1 Knapsack.cpp
+++ 038 Dynamic Programming 5/A2 0-1 Knapsack.cpp
@@ -69,3 +69,31 @@ Explanation 2:
 
 
 */
+
+int knapsack(vector<int>&A,vector<int>&B,int n,int C)
+{
+    int dp[n+1][C+1];
+    
+    for(int i=0;i<n+1;i++)
+        for(int j=0;j<C+1;j++)
+            if(i==0||j==0)
+                dp[i][j]=0;
+    
+    for(int i=1;i<n+1;i++)
+    for(int j=1;j<C+1;j++)
+    {
+        if(B[i-1]<=j)
+        {
+            dp[i][j] = max(A[i-1] + dp[i-1][j-B[i-1]],dp[i-1][j]);
+        }else 
+            dp[i][j] = dp[i-1][j];
+    }
+    
+    return dp[n][C];
+}
+
+int Solution::solve(vector<int> &A, vector<int> &B, int C) {
+    int n = A.size();
+    
+    return knapsack(A,B,n,C);
+}
