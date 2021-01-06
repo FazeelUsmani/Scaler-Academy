d1931371c79be2776f934c8f6e17660305ca879b
FazeelUsmani <fazeelusmani18@gmail.com>
2021-01-06 19:08:04 +0530

    added solution

--- Contests/02 Contest/04 A-B.cpp
+++ Contests/02 Contest/04 A-B.cpp
@@ -1,4 +1,6 @@
+/*
 A - B
+
 Problem Description
 Given an array A of integers of size N. Find the size of the largest subset of the array in which every pair satisfy the condition: x2 + y2 + x*y ≡ B mod C, where x and y are two different elements of the subset and C is a prime number. Note: All elements in the array are different.     
 
@@ -31,3 +33,26 @@ Example Output
 
 Example Explanation
 No pair exist in the array which satisfy the condition so, we can only take one element in the subset.
+*/
+
+#define ll long long
+int Solution::solve(vector<int> &A, int B, int C)
+{
+
+    unordered_map<ll, ll> umap;
+    ll ans = 0;
+    ll K = B;
+    ll M = C;
+    int n = A.size();
+    for (int i = 0; i < n; ++i)
+    {
+        ll d = (((1LL * A[i] * A[i]) % M) * A[i]) % M - (A[i] * K) % M;
+        d %= M;
+        if (d < 0)
+            d += M;
+        umap[d]++;
+        ans = max(ans, umap[d]);
+    }
+
+    return ans;
+}
