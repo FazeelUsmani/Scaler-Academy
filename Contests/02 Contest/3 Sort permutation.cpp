9e7ef33c217ecaf911d033f4347d536edc69bbf9
FazeelUsmani <fazeelusmani18@gmail.com>
2021-01-07 14:56:54 +0530

    variation of minSwaps2

--- Contests/02 Contest/3 Sort permutation.cpp
+++ Contests/02 Contest/3 Sort permutation.cpp
@@ -1,4 +1,6 @@
+/*
 Sort the permutation
+
 Problem Description
 Given a permutation of size N. Each number has some color associated to it.
 Find the maximum number of colors that can be used such that you are able to sort the permutation by swapping the elements having same color.
@@ -32,3 +34,21 @@ We can color 4, 2, 3 with same color and 1 with different color.
 Given A = [1,4,2,3].
 After swapping 4 and 2,  A => [1,2,4,3].
 Now swap 4 and 3 to get the sorted permutation A => [1,2,3,4].
+*/
+
+// Similar to minSwaps2
+int Solution::solve(vector<int> &A)
+{
+
+    int ans = 0;
+    for (int i = 0; i < A.size(); ++i)
+    {
+        while (A[i] != (i + 1))
+        {
+            ans++;
+            swap(A[i], A[A[i] - 1]);
+        }
+    }
+
+    return A.size() - ans;
+}
